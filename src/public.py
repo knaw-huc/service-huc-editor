fba0e63a8d203aa3f567a627272af477bbfc45a0
Menzo Windhouwer <menzo@windhouwer.nl>
2024-09-25 10:26:47 +0200

    adding pdf to record endpoint

--- src/public.py
+++ src/public.py
@@ -3,12 +3,13 @@ import os
 import json
 import pathlib
 
-from enum import Enum
 from fastapi import APIRouter, HTTPException
 from starlette import status
 from starlette.requests import Request
 from starlette.responses import JSONResponse, HTMLResponse, Response
 from fastapi.encoders import jsonable_encoder
+from enum import Enum
+from weasyprint import HTML
 
 from saxonche import PySaxonProcessor, PyXdmValue, PySaxonApiError
 
@@ -140,66 +141,31 @@ async def read_app(request: Request, app: str):
             
     return HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED)
 
-class RecForm(str, Enum):
-    json = "json"
-    xml = "xml"
-    html = "html"
-    pdf = "pdf"
-
-@router.get('/app/{app}/record/{nr}')
-@router.get('/app/{app}/record/{nr}/{form}')
-def get_record(request: Request, app: str, nr: str, form: RecForm | None = RecForm.xml):
-    """
-    Endpoint to get a record based on its ID and the application name.
-    This endpoint accepts the application name and the ID as path parameters.
-    If the record does not exist, it returns a 404 error.
-    If the record exists but the reading functionality is not implemented yet, it returns a 501 error.
-    """
-    logging.info(f"app[{app}] record[{nr}] form[{form}]")
+def rec_html(app,nr):
     record_file = f"{settings.URL_DATA_APPS}/{app}/records/record-{nr}.xml"
-
-    if not os.path.exists(record_file):
-        logging.debug(f"{record_file} doesn't exist")
-        return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    
     with open(record_file, 'r') as file:
         rec = file.read()
-
-        if form == RecForm.json or "application/json" in request.headers.get("accept", ""):
-            with PySaxonProcessor(license=False) as proc:
-                node = proc.parse_xml(xml_text=rec)
-                xpproc = proc.new_xpath_processor()
-                xpproc.set_cwd(os.getcwd())
-                xpproc.declare_namespace('clariah','http://www.clariah.eu/')
-                xpproc.declare_namespace('cmd','http://www.clarin.eu/cmd/')
-                xpproc.set_context(xdm_item=node)
-                prof = xpproc.evaluate_single("string(/cmd:CMD/cmd:Header/cmd:MdProfile)").get_string_value()
-                prof_doc = prof_json(prof)
-                xsltproc = proc.new_xslt30_processor()
-                xsltproc.set_cwd(os.getcwd())
-                executable = xsltproc.compile_stylesheet(stylesheet_file=f"{settings.xslt_dir}/rec2json.xsl")
-                executable.set_parameter("prof-doc", proc.make_string_value(prof_doc))
-                node = proc.parse_xml(xml_text=rec)
-                result = executable.transform_to_string(xdm_node=node)
-                return JSONResponse(content=jsonable_encoder(json.loads(result)))
-        elif form == RecForm.html or "text/html" in request.headers.get("accept", ""):
-            with PySaxonProcessor(license=False) as proc:
-                xsltproc = proc.new_xslt30_processor()
-                xsltproc.set_cwd(os.getcwd())
-                executable = xsltproc.compile_stylesheet(stylesheet_file=f"{settings.xslt_dir}/toHTML.xsl")
-                executable.set_parameter("cwd", proc.make_string_value(os.getcwd()))
-                executable.set_parameter("base", proc.make_string_value("http://localhost:1210"))
-                executable.set_parameter("app", proc.make_string_value(app))
-                executable.set_parameter("nr", proc.make_string_value(nr))
-                config = proc.parse_xml(xml_file_name=f"{settings.URL_DATA_APPS}/{app}/config.xml")
-                executable.set_parameter("config", config)
-                null = proc.parse_xml(xml_text=rec)
-                result = executable.transform_to_string(xdm_node=null)
-                return HTMLResponse(content=result)
-        elif form == RecForm.xml or "application/xml" in request.headers.get("accept", ""):
-            return Response(content=rec, media_type="application/xml")
-
-    return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not supported")
+        with PySaxonProcessor(license=False) as proc:
+            rec = proc.parse_xml(xml_text=rec)
+            xpproc = proc.new_xpath_processor()
+            xpproc.set_cwd(os.getcwd())
+            xpproc.declare_namespace('clariah','http://www.clariah.eu/')
+            xpproc.declare_namespace('cmd','http://www.clarin.eu/cmd/')
+            xpproc.set_context(xdm_item=rec)
+            prof = xpproc.evaluate_single("string(/cmd:CMD/cmd:Header/cmd:MdProfile)").get_string_value()
+            prof = prof_xml(prof)
+            xsltproc = proc.new_xslt30_processor()
+            xsltproc.set_cwd(os.getcwd())
+            executable = xsltproc.compile_stylesheet(stylesheet_file=f"{settings.xslt_dir}/toHTML.xsl")
+            executable.set_parameter("cwd", proc.make_string_value(os.getcwd()))
+            executable.set_parameter("base", proc.make_string_value("http://localhost:1210"))
+            executable.set_parameter("app", proc.make_string_value(app))
+            executable.set_parameter("nr", proc.make_string_value(nr))
+            prof = proc.parse_xml(xml_text=prof)
+            executable.set_parameter("tweak-doc",prof) 
+            config = proc.parse_xml(xml_file_name=f"{settings.URL_DATA_APPS}/{app}/config.xml")
+            executable.set_parameter("config", config)
+            return executable.transform_to_string(xdm_node=rec)
 
 @router.get('/app/{app}/record/{nr}/editor')
 def get_editor(request: Request, app: str, nr: str):
@@ -220,6 +186,65 @@ def get_editor(request: Request, app: str, nr: str):
             result = executable.transform_to_string(xdm_node=null)
             return HTMLResponse(content=result)
  
+class RecForm(str, Enum):
+    json = "json"
+    xml = "xml"
+    html = "html"
+    pdf = "pdf"
+
+@router.get('/app/{app}/record/{nr}')
+@router.get('/app/{app}/record/{nr}/{form}')
+def get_record(request: Request, app: str, nr: str, form: RecForm | None = RecForm.html):
+    """
+    Endpoint to get a record based on its ID and the application name.
+    This endpoint accepts the application name and the ID as path parameters.
+    If the record does not exist, it returns a 404 error.
+    If the record exists but the reading functionality is not implemented yet, it returns a 501 error.
+    """
+    logging.info(f"app[{app}] record[{nr}] form[{form}] accept[{request.headers.get("accept", "")}]")
+    record_file = f"{settings.URL_DATA_APPS}/{app}/records/record-{nr}.xml"
+
+    if not os.path.exists(record_file):
+        logging.debug(f"{record_file} doesn't exist")
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    if form == RecForm.json or "application/json" in request.headers.get("accept", ""):
+        with PySaxonProcessor(license=False) as proc:
+            rec = proc.parse_xml(xml_file_name=record_file)
+            xpproc = proc.new_xpath_processor()
+            xpproc.set_cwd(os.getcwd())
+            xpproc.declare_namespace('clariah','http://www.clariah.eu/')
+            xpproc.declare_namespace('cmd','http://www.clarin.eu/cmd/')
+            xpproc.set_context(xdm_item=rec)
+            prof = xpproc.evaluate_single("string(/cmd:CMD/cmd:Header/cmd:MdProfile)").get_string_value()
+            prof_doc = prof_json(prof)
+            xsltproc = proc.new_xslt30_processor()
+            xsltproc.set_cwd(os.getcwd())
+            executable = xsltproc.compile_stylesheet(stylesheet_file=f"{settings.xslt_dir}/rec2json.xsl")
+            executable.set_parameter("prof-doc", proc.make_string_value(prof_doc))
+            result = executable.transform_to_string(xdm_node=rec)
+            return JSONResponse(content=jsonable_encoder(json.loads(result)))
+    elif form == RecForm.pdf or "application/pdf" in request.headers.get("accept", ""):
+            html = rec_html(app,nr)
+            pdf = HTML(string=html).write_pdf()
+            headers = {'Content-Disposition': f'inline; filename="{app}-record-{nr}.pdf"'}
+            return Response(pdf, headers=headers, media_type='application/pdf')
+    # FF sends an Accept header with text/html and application/xml, we prefer html, 
+    # but first check for an explicit xml request
+    elif form == RecForm.xml:
+        with open(record_file, 'r') as file:
+            rec = file.read()
+            return Response(content=rec, media_type="application/xml")
+    elif form == RecForm.html or "text/html" in request.headers.get("accept", ""):
+            html = rec_html(app,nr)
+            return HTMLResponse(content=html)
+    elif form == RecForm.xml or "application/xml" in request.headers.get("accept", ""):
+        with open(record_file, 'r') as file:
+            rec = file.read()
+            return Response(content=rec, media_type="application/xml")
+
+    return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not supported")
+
 @router.get('/{app_name}/record/{id}/resource/{resource_id}')
 def get_record_resource(app_name: str, id: str, resource_id: str):
     """
